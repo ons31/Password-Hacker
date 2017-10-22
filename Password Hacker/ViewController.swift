8cd64f9205bfa3e070bdf59a80ec80ef6de8e5cb
ons31 <32807195+ons31@users.noreply.github.com>
2017-10-22 14:26:32 +0900

    First Commit

--- Password Hacker/ViewController.swift
+++ Password Hacker/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var resultLabel1: UILabel!
+    @IBOutlet var resultLabel2: UILabel!
+    @IBOutlet var resultLabel3: UILabel!
+    @IBOutlet var resultLabel4: UILabel!
+    
+//count
+    @IBOutlet  var pressLabel: UILabel!
+    
+//pw
+    var password: Int = 1234
+    
+//start button
+    @IBAction func start() {
+        for i in 0...9999 {
+            pressLabel.text = String(i)
+        RunLoop.main.run(until: Date(timeIntervalSinceNow: 0.000005))
+        
+        if password == i {
+            
+            var digits = [Int]()
+            
+            for _ in 0...3 {
+                digits.append(password % 10)
+                password /= 10
+                
+                resultLabel1.text = String(digits[0])
+                resultLabel2.text = String(digits[0])
+                resultLabel3.text = String(digits[0])
+                resultLabel4.text = String(digits[0])
+                
+                print("\(i) is the password")
+                
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+//reset button
+    func reset() {
+        password = 1234
+        pressLabel.text = "PRESS START"
+        
+        resultLabel1.text = "0"
+        resultLabel2.text = "0"
+        resultLabel3.text = "0"
+        resultLabel4.text = "0"
+        
+        }
     }
-
-
 }
-
